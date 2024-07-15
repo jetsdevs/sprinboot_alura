baf64374b2c74fa7871d3bed68cc28dd66b80e09
jetesdevs <jeugeniotomaz.dev@gmail.com>
2024-07-15 17:17:13 -0300

    Upadate_properties

--- src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package med.voll.api.infra.security;
 
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
